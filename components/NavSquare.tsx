e7c3c077d844a0cfa7ca25f840a7b09ddb264880
Danimal <danimal@hackpoetic.com>
2023-05-14 18:13:39 -0400

    Remove unused callback

--- components/NavSquare.tsx
+++ components/NavSquare.tsx
@@ -6,18 +6,17 @@ import React from 'react';
 type NavSquareProps = {
   title: string
   description: string
-  onClick?: () => void
 }
 
-export const NavSquare = ({ title, description, onClick }: NavSquareProps) => {
+export const NavSquare = ({ title, description }: NavSquareProps) => {
   return (
-    <button onClick={onClick} className="flex items-center bg-purple-500 text-white py-4 px-5 hover:cursor-pointer hover:bg-purple-600 flex flex-col gap-2">
+    <div className="flex items-center w-full h-full bg-purple-500 text-white py-4 px-5 hover:cursor-pointer hover:bg-purple-600 flex flex-col gap-2">
       <div className="font-bold text-2xl font-header">
         {title}
       </div>
       <div className="text-xl ml-2 font-body">
         {description}
       </div>
-    </button>
+    </div>
   )
 }
