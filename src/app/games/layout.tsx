d2fcab4c33d9d5c94f069a03cffb301e62bd8ffa
Danimal <danimal@hackpoetic.com>
2023-05-21 13:33:48 -0400

    tweak header color

--- src/app/games/layout.tsx
+++ src/app/games/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <div className="flex flex-wrap-reverse justify-center items-center p-4 min-h-screen">
       <div className="flex-1">
         <div className="p-8 flex flex-col items-center">
-          <Link href="/" className="text-3xl font-bold font-header text-amber-500 mb-6 whitespace-nowrap">Tactical Elements</Link>
+          <Link href="/" className="text-3xl font-bold font-header text-amber-300 mb-6 whitespace-nowrap">Tactical Elements</Link>
           <SiteNav />
         </div>
       </div>
