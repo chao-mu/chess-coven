3ebaaca349c24fb8a078d6100d3f0a6002c8bf5e
Danimal <danimal@hackpoetic.com>
2023-05-23 11:22:50 -0400

    functional hotloading

--- src/stories/ChessboardSquare.stories.ts
+++ src/stories/ChessboardSquare.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 
 // Chess.js
 import { BLACK, WHITE, QUEEN, Piece } from 'chess.js'
