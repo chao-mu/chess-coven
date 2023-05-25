ed0fa7e944db9d8d3d1d006ae192868482b65879
Danimal <danimal@hackpoetic.com>
2023-05-25 12:44:15 -0400

    increse text size in game start screen

--- src/components/GameStartScreen.tsx
+++ src/components/GameStartScreen.tsx
@@ -11,11 +11,11 @@ type GameStartScreenProps = {
 export const GameStartScreen = ({ rules, story, onGameStart, title }: GameStartScreenProps) => {
   return (
     <div className="flex flex-col p-6">
-      <h1 className="mb-4 text-center text-4xl font-bold">{ title }</h1>
-      <div className="mb-1 text-center font-bold">Story</div>
-      <p className="mb-4 indent-4">{ story }</p>
-      <div className="mb-1 text-center font-bold">Rules</div>
-      <p className="mb-4 indent-4">{ rules }</p>
+      <h1 className="mb-4 text-center text-4xl">{ title }</h1>
+      <div className="mb-1 text-center text-xl font-bold">Story</div>
+      <p className="mb-4 indent-4 text-lg">{ story }</p>
+      <div className="mb-1 text-center text-xl font-bold">Rules</div>
+      <p className="mb-4 indent-4 text-lg">{ rules }</p>
       <div className="flex justify-center">
         <button className="rounded bg-amber-600 px-4 py-2 text-white hover:bg-amber-700" onClick={onGameStart}>Play</button>
       </div>
