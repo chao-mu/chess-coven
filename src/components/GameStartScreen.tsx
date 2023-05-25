d49ab5053c9eb92622edbe9d3a96b8bc62aaa186
Danimal <danimal@hackpoetic.com>
2023-05-25 12:46:25 -0400

    increase indent

--- src/components/GameStartScreen.tsx
+++ src/components/GameStartScreen.tsx
@@ -13,9 +13,9 @@ export const GameStartScreen = ({ rules, story, onGameStart, title }: GameStartS
     <div className="flex flex-col p-6">
       <h1 className="mb-4 text-center text-4xl">{ title }</h1>
       <div className="mb-1 text-center text-xl font-bold">Story</div>
-      <p className="mb-4 indent-4 text-lg">{ story }</p>
+      <p className="mb-4 indent-6 text-lg">{ story }</p>
       <div className="mb-1 text-center text-xl font-bold">Rules</div>
-      <p className="mb-4 indent-4 text-lg">{ rules }</p>
+      <p className="mb-4 indent-6 text-lg">{ rules }</p>
       <div className="flex justify-center">
         <button className="rounded bg-amber-600 px-4 py-2 text-white hover:bg-amber-700" onClick={onGameStart}>Play</button>
       </div>
