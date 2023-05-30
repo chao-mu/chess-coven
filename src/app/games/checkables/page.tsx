dd016fe44c4437bc37468f3a03c37e8b60ec3da1
Danimal <danimal@hackpoetic.com>
2023-05-30 18:39:17 -0400

    only auto-advance in certain puzzles

--- src/app/games/checkables/page.tsx
+++ src/app/games/checkables/page.tsx
@@ -1,23 +1,25 @@
-"use client"
+"use client";
 
 // React
-import React from 'react'
+import React from "react";
 
 // Components
-import { SolutionClicker } from '@/components/SolutionClicker';
+import { SolutionClicker } from "@/components/SolutionClicker";
 
 // Assets
-import checkables from '@/assets/puzzles/checkables.json';
+import checkables from "@/assets/puzzles/checkables.json";
 
+const TITLE = "Miserable Kings";
 
-const TITLE = "Misarable King"
+const RULES =
+  "Click every piece whose move can result in a check in the current position for either side.";
 
-const RULES = "Select every piece and pawn that could potentially be moved such that it checks its enemy king. Once all solutions are provided, the level will advance."
-
-const STORY = "The monarchies of the world have been overthrown by the people. The kings have been imprisoned in their own castles. The people have decided to let the monarchs live, but they must be kept in check."
+const STORY =
+  "The monarchies of the world have been overthrown by the people. The kings have been imprisoned in their own castles. The people have decided to let the monarchs live, but they must be kept in check.";
 
 export default function Page() {
-  const randomPuzzle = () => checkables[Math.floor(Math.random() * checkables.length)];
+  const randomPuzzle = () =>
+    checkables[Math.floor(Math.random() * checkables.length)];
   return (
     <div className="flex flex-col bg-gray-800/50">
       <SolutionClicker
@@ -25,7 +27,8 @@ export default function Page() {
         title={TITLE}
         story={STORY}
         rules={RULES}
+        autoAdvance={false}
       />
     </div>
-  )
+  );
 }
--- src/app/games/undefended/page.tsx
+++ src/app/games/undefended/page.tsx
@@ -1,23 +1,25 @@
-"use client"
+"use client";
 
 // React
-import React from 'react'
+import React from "react";
 
 // Components
-import { SolutionClicker } from '@/components/SolutionClicker';
+import { SolutionClicker } from "@/components/SolutionClicker";
 
 // Assets
-import undefendedPieces from '@/assets/puzzles/undefended.json';
+import undefendedPieces from "@/assets/puzzles/undefended.json";
 
+const TITLE = "Enchant the Undefended";
 
-const TITLE = "Enchant the Undefended"
+const RULES =
+  "Click undefended pieces and pawns. Next puzzle will show once all solutions are selected.";
 
-const RULES = "Click undefended pieces and pawns. Next puzzle will show once all solutions are selected."
-
-const STORY = "The battlefield is littered with fallen chesspersons. Opposing forces clash, blinded by mutual hatred and pricked on by a thirst for blood. There is however a chance for peace. Find the chesspersons who are most vulnerable and pacify them to quell the cycle of violence."
+const STORY =
+  "The battlefield is littered with fallen chesspersons. Opposing forces clash, blinded by mutual hatred and pricked on by a thirst for blood. There is however a chance for peace. Find the chesspersons who are most vulnerable and pacify them to quell the cycle of violence.";
 
 export default function Page() {
-  const randomPuzzle = () => undefendedPieces[Math.floor(Math.random() * undefendedPieces.length)];
+  const randomPuzzle = () =>
+    undefendedPieces[Math.floor(Math.random() * undefendedPieces.length)];
   return (
     <div className="flex flex-col bg-gray-800/50">
       <SolutionClicker
@@ -25,7 +27,8 @@ export default function Page() {
         title={TITLE}
         story={STORY}
         rules={RULES}
+        autoAdvance={false}
       />
     </div>
-  )
+  );
 }
