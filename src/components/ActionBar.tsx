d308e291c78c4142dbb40a5747c0a027e651556b
hackpoetic <chao-mu@hackpoetic.com>
2023-06-02 21:53:12 -0400

    Feature/UI revamp (#14)
    * include correct/incorrect guesses in actionbar
    
    * Group UI elements, add more rules, padding
    
    ---------
    
    Co-authored-by: Danimal <danimal@hackpoetic.com>

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -14,6 +14,8 @@ export type ActionBarProps = {
   onGiveUp?: () => void;
   onSanEntry: (san: string) => void;
   sanEntry: boolean;
+  goodGuesses?: string[];
+  badGuesses?: string[];
 };
 
 export const ActionBar = ({
@@ -22,37 +24,61 @@ export const ActionBar = ({
   onGiveUp,
   onAdvance,
   onSanEntry,
+  goodGuesses = [],
+  badGuesses = [],
   sanEntry = false,
 }: ActionBarProps) => {
   return (
-    <div className="flex flex-wrap items-start justify-center gap-4">
+    <div className="flex flex-wrap items-start justify-between gap-4 p-4">
       {sanEntry && (
         <SanInputForm
           onSubmit={onSanEntry}
           isWrong={playerStatus == "wrong-guess"}
         />
       )}
-      {playerStatus == "premature-advancement" && (
-        <div className="p-2 text-center text-xl font-bold text-amber-400">
-          Still more to go!
-        </div>
-      )}
-      {playerStatus != "gave-up" && (
-        <button
-          className="whitespace-nowrap rounded bg-amber-600 px-4 py-2 font-bold text-white hover:bg-amber-700"
-          onClick={onGiveUp}
-        >
-          Give Up
-        </button>
-      )}
-      {(!autoAdvance || playerStatus == "gave-up") && (
-        <button
-          className="rounded bg-amber-600 px-4 py-2 font-bold text-white hover:bg-amber-700"
-          onClick={onAdvance}
-        >
-          Advance
-        </button>
-      )}
+      <div className="flex flex-wrap gap-2 p-2">
+        {goodGuesses && goodGuesses.length > 0 && (
+          <div className="flex gap-2">
+            {goodGuesses.map((guess) => (
+              <div className="text-green-500" key={guess}>
+                {guess}
+              </div>
+            ))}
+          </div>
+        )}
+        {badGuesses && badGuesses.length > 0 && (
+          <div className="flex gap-2 line-through">
+            {badGuesses.map((guess) => (
+              <div className="text-red-500" key={guess}>
+                {guess}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="flex gap-2">
+        {playerStatus == "premature-advancement" && (
+          <div className="p-2 text-center text-xl font-bold text-amber-400">
+            Still more to go!
+          </div>
+        )}
+        {playerStatus != "gave-up" && (
+          <button
+            className="whitespace-nowrap rounded bg-amber-600 px-4 py-2 font-bold text-white hover:bg-amber-700"
+            onClick={onGiveUp}
+          >
+            Give Up
+          </button>
+        )}
+        {(!autoAdvance || playerStatus == "gave-up") && (
+          <button
+            className="rounded bg-amber-600 px-4 py-2 font-bold text-white hover:bg-amber-700"
+            onClick={onAdvance}
+          >
+            Advance
+          </button>
+        )}
+      </div>
     </div>
   );
 };
