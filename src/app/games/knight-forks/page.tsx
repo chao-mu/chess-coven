fd06b75861cae596b0380fb473b59e7d7bec442b
Danimal <danimal@hackpoetic.com>
2023-06-15 11:14:45 -0400

    spelling fix

--- src/app/games/knight-forks/page.tsx
+++ src/app/games/knight-forks/page.tsx
@@ -15,7 +15,7 @@ const RULES =
   "Click squares that would create a knight fork. In other words, click all squares that are a knight distance from both the king and queen.";
 
 const STORY =
-  "The king and queen of the enemy army think they're safe chilling at the local outdoor pub. Catapult a knight across the kingdom (it's a Tactical Elements brand catapult) and cause mayham!";
+  "The king and queen of the enemy army think they're safe chilling at the local outdoor pub. Catapult a knight across the kingdom (it's a Tactical Elements brand catapult) and cause mayhem!";
 
 export default function Page() {
   const randomPuzzle = () =>
