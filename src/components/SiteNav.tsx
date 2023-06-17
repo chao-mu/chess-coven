de6a443b915abbfe19f1c316e3bcc8c25cd7be3a
Danimal <danimal@hackpoetic.com>
2023-06-17 10:56:38 -0400

    touch up menu

--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -8,11 +8,12 @@ import { LinkButton } from "@/components/LinkButton";
 
 export function SiteNav() {
   return (
-    <div className="flex max-w-2xl flex-col gap-2">
-      <LinkButton href="/games/knight-forks">Catapult Knights</LinkButton>
-      <LinkButton href="/games/checkables">Miserable Kings</LinkButton>
-      <LinkButton href="/games/undefended">The Undefended</LinkButton>
+    <div className="flex flex-col gap-2">
+      <LinkButton href="/games/knight-forks">Knight Forks</LinkButton>
       <LinkButton href="/games/checks-captures">Checks/Captures</LinkButton>
+      <LinkButton href="/games/undefended">Undefended Pieces</LinkButton>
+      <div className="border-b-2 border-amber-300">
+      </div>
       <LinkButton href="/games/memorizer">Game Memorizer</LinkButton>
     </div>
   );
