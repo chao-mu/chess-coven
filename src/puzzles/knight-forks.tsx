fa544be6dd0f6fd44348b2d30578f51efa9cabfc
Danimal <danimal@hackpoetic.com>
2023-06-17 18:43:07 -0400

    add puzzles back in

--- src/puzzles/knight-forks.tsx
+++ src/puzzles/knight-forks.tsx
@@ -1,9 +1,12 @@
 import { PuzzleCollection } from '@/types'
 
+import puzzles from '@/assets/puzzles/knight-forkables.json'
+
 export default {
   title: "Catapult Knights",
   rules: "Click squares that would create a knight fork. In other words, click all squares that are a knight distance from both the king and queen.",
   story: "The king and queen of the enemy army think they're safe chilling at the local outdoor pub. Catapult a knight across the kingdom (it's a Tactical Elements brand catapult) and cause mayhem!",
   autoAdvance: true,
   solutionType: "square",
+  puzzles: puzzles,
 } as PuzzleCollection
