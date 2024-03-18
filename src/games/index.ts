9d6111aca20efcb2f77c463508ee7e1351274e87
Danimal <danimal@hackpoetic.com>
2024-03-18 14:48:48 -0400

    fix: knight game only mentions clicking single square

--- src/games/index.ts
+++ src/games/index.ts
@@ -25,7 +25,7 @@ const games: Record<string, GameInfo & { collection: PuzzleCollection }> = {
     flavor: {
       title: "Catapult Knights",
       rules:
-        "Click a square that if you were to drop a knight on it, all pieces would be attacked. Otherwise click No Solution.",
+        "Click all squares that attack all pieces simultaneously. Otherwise click No Solution.",
       story:
         "The king and queen of the enemy army think they're safe chilling at the local outdoor pub. Catapult a knight across the kingdom (it's a Chess Coven brand catapult) and cause mayhem!",
     },
@@ -35,8 +35,7 @@ const games: Record<string, GameInfo & { collection: PuzzleCollection }> = {
     logic: { autoAdvance: false, solutionType: "move", noSolution: false },
     flavor: {
       title: "Checks/Captures",
-      rules:
-        "By clicking squares, perform all legal captures and checks for either side.",
+      rules: "Perform all legal captures and checks for either side.",
       story:
         "Tomorrow the war reaches your small village. Analyze the crawling battlefield to determine where destruction is inevitable.",
     },
@@ -46,8 +45,7 @@ const games: Record<string, GameInfo & { collection: PuzzleCollection }> = {
     logic: { autoAdvance: true, solutionType: "number", noSolution: false },
     flavor: {
       title: "Count Capture Points",
-      rules:
-        "Watch the animation and determine the final change in material difference after all captures, using relative piece values. Captures of Pawns are worth 1 point, Knights and Bishops are worth 3 points, Rooks are worth 5 points, and Queens are worth 9 points. For example, if a pawn captures a knight and then the pawn is captured, the answer would be 2. If a queen captures a pawn and a pawn takes back, the total is -8",
+      rules: "Watch the animation and determine the change in material value.",
       story:
         "The death toll from the war increases every day. The war has gone on far too long and soon will be the mutual ruin of both kingdoms. You must prepare a PowerPoint slide presentation to convince the ruling monarchs that peace is the best option.",
     },
