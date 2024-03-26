01d89d08d06744d87e334e87b741494c384c4778
Danimal <danimal@hackpoetic.com>
2024-03-25 21:19:03 -0400

    refactor: remove memorizer game

--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -31,7 +31,6 @@ export function SiteNav() {
         </LinkButton>
       ))}
       <div className="min-h-0 border-b-2 border-amber-300"></div>
-      <LinkButton href="/games/memorizer">Game Memorizer</LinkButton>
       <LinkButton href="https://www.patreon.com/user?u=84821837">
         Help Cover Costs
       </LinkButton>
